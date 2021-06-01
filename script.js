3060d904d944d2f08747c9bc68f83af7507550c8
Gen <example@gmail.com>
2021-06-01 23:49:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let arr = ['tola', 'ola', 5, 'motiv'];
+
+// console.log(arr[3]);
+
+// alert('privet');
+
+// const result = confirm("are you here?");
+
+// console.log(result);
+
+// 
+
+// 
+
+const user = "Ivan";
+
+alert( `Privet, ${user}`);
